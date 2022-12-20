6f86c07557c83bd60db9ec46d02e3423fc59e43d
jonpfluger <jonpfluger718@gmail.com>
2022-12-20 11:07:35 -0600

    added PUT and DELETE routes in blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const {Blog} = require('../../models')
 
-// POST route
+// POST route (create)
 router.post('/', async (req, res) => {
     try {
         const newBlog = await Blog.create({
@@ -14,6 +14,34 @@ router.post('/', async (req, res) => {
     }
 })
 
-// edit route
+// PUT route (edit)
+router.put('/:id', async (req, res) => {
+    try {
+      const updatedBlog = await Blog.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      })
+      res.status(200).json(updatedBlog)
+    } catch (err) {
+      res.status(400).json(err);
+    }
+})
+
+// DELETE route
+router.delete('/:id', async (req, res) => {
+    try {
+      const deletedBlog = await Blog.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        }
+      })
+      
+      res.status(200).json(deletedBlog)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+})
 
 module.exports = router
