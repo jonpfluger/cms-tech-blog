a9bc4a2982e789b70a5b42848393838f1dec9726
jonpfluger <jonpfluger718@gmail.com>
2022-12-22 18:07:01 -0600

    edit post, footer

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Description
 
-This application lets a user create an account, read posts about tech, and write their own posts. 
+This application lets a user create an account, read posts about tech, and write their own posts.
+
+Writing about tech can be just as important as making it. Developers spend plenty of time creating new applications and debugging existing codebases, but most developers also spend at least some of their time reading and writing about technical concepts, recent advancements, and new technologies.
 
 ## Screenshot
 
--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,1 +1,34 @@
-// use put in fetch
+const editPost = document.getElementById('editPost')
+
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const {
+        title: titleInput,
+        content: contentInput,
+      } = event.target.elements
+    
+      const updatedPost = {
+        title: titleInput.value,
+        content: contentInput.value
+      }
+
+    const blogId = event.target.dataset.blogid
+
+    fetch(`/api/blogs/${blogId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedPost)
+      })
+      .then(response => {
+        if (response.status === 200) {
+          window.location.href = '/dashboard'
+        }
+      })
+      .catch(err => console.log(err))
+}
+
+editPost.addEventListener('submit', handleSubmit)
